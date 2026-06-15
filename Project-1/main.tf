b17404d9493aee872df682e50b912e7e99024928
prajvalg2110-max <prajvalg2110@gmail.com>
2026-06-15 08:45:40 +0530

    added root files

---
+++ Project-1/main.tf
@@ -0,0 +1,31 @@
+module "ec2" {
+    source = "./modules/ec2"
+
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    instance_name = var.instance_name
+
+
+    subnet_id = module.subnet.subnet_id
+    security_group_id = module.security_group.security_group_id
+}
+
+module "vpc" {
+    source = "./modules/vpc
+
+    vpc_cidr = var.vpc_cidr
+    vpc_name = var.vpc_name
+}
+
+module "subnet {
+    source = "./modules/subnet
+    subnet_cidr = var.subnet_cidr
+    subnet_name = var.subnet_name
+    vpc_id = module.vpc.vpc_id
+}
+module "security_group" {
+    source = "./modules/security_group
+
+    security_group_name = var.security_group_name
+    vpc_id = module.vpc.vpc_id
+}
---
+++ Project-1/variables.tf
@@ -0,0 +1,31 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "security_group_name" {
+  type = string
+}
