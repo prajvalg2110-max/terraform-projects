09d443a6b4462e3d48d28c235e8752e0974bd7c7
root <root@ip-172-31-10-207.ap-south-2.compute.internal>
2026-06-15 04:01:08 +0000

    added backend setup

---
+++ backend-setup/main.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "prajval-terraform-state-ap-south-2-2026"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
